1641a598c80ab62118ffbd859dedd947f9bc9c38
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-29 21:18:31 +0530

    "ads2_m1_a1_1: 1/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M1/1.1 Graph/Solution.java
+++ M1/1.1 Graph/Solution.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 interface Graph {
-    public int V();
-    public int E();
-    public void addEdge(int v, int w);
-    public Iterable<Integer> adj(int v);
-    public boolean hasEdge(int v, int w);
+	public int V();
+	public int E();
+	public void addEdge(int v, int w);
+	public Iterable<Integer> adj(int v);
+	public boolean hasEdge(int v, int w);
 }
 
 class AdjMatrixGraph implements Graph {
@@ -17,41 +17,41 @@ class AdjMatrixGraph implements Graph {
 		matrix = new int[vertical][vertical];
 	}
 
-    public int V() {
-    	return vertices;
-    }
+	public int V() {
+		return vertices;
+	}
 
-    public int E() {
-    	return edges;
-    }
+	public int E() {
+		return edges;
+	}
 
-    public void addEdge(int v, int w) {
-    	matrix[v][w] = 1;
-    	matrix[w][v] = 1;
-    	edges++;
+	public void addEdge(int v, int w) {
+		matrix[v][w] = 1;
+		matrix[w][v] = 1;
+		edges++;
 
-    }
+	}
 
-    public Iterable<Integer> adj(int v) {
-    	return null;
-    }
+	public Iterable<Integer> adj(int v) {
+		return null;
+	}
 
-    public boolean hasEdge(int v, int w) {
-    	return matrix[v][w] == 1;
-    }
+	public boolean hasEdge(int v, int w) {
+		return matrix[v][w] == 1;
+	}
 
-    public void print() {
-    	System.out.println(vertices+" vertices, "+edges+" edges");
+	public void print() {
+		System.out.println(vertices + " vertices, " + edges + " edges");
 
-    	for(int i = 0; i < vertices; i++) {
-    		String str = "";
-    		for(int j = 0; j < edges; j++){
-    			str += matrix[i][j]+" ";
+		for (int i = 0; i < edges; i++) {
+			String str = "";
+			for (int j = 0; j < vertices; j++) {
+				str += matrix[i][j] + " ";
 
-    		}
-    		System.out.println(str);
-    	}
-    }
+			}
+			System.out.println(str);
+		}
+	}
 
 
 
@@ -67,20 +67,20 @@ final class Solution {
 		int vertexNum = Integer.parseInt(scan.nextLine());
 		int edgenum = Integer.parseInt(scan.nextLine());
 		String[] places = scan.nextLine().split(",");
-        switch (cmd) {
+		switch (cmd) {
 		case "List" :
 
 			break;
 		case "Matrix" :
 
-		     AdjMatrixGraph obj = new AdjMatrixGraph(vertexNum);
+			AdjMatrixGraph obj = new AdjMatrixGraph(vertexNum);
 
-		     for(int i = 0; i < edgenum; i++) {
-		     	String[] input = scan.nextLine().split(" ");
-		     	obj.addEdge(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
-             }
-             obj.print();
-             break;
+			for (int i = 0; i < edgenum; i++) {
+				String[] input = scan.nextLine().split(" ");
+				obj.addEdge(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+			}
+			obj.print();
+			break;
 
 		}
 
