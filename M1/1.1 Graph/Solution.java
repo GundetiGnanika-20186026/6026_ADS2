3ec65359e1bd90538eada3aa1def7c7e32bebfcb
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-29 21:13:35 +0530

    "ads2_m1_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M1/1.1 Graph/Solution.java
+++ M1/1.1 Graph/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 interface Graph {
     public int V();
     public int E();
@@ -5,4 +6,90 @@ interface Graph {
     public Iterable<Integer> adj(int v);
     public boolean hasEdge(int v, int w);
 }
+
+class AdjMatrixGraph implements Graph {
+	private int vertices;
+	private int edges;
+	private int[][] matrix;
+	AdjMatrixGraph(int vertical) {
+		this.vertices = vertical;
+		this.edges = 0;
+		matrix = new int[vertical][vertical];
+	}
+
+    public int V() {
+    	return vertices;
+    }
+
+    public int E() {
+    	return edges;
+    }
+
+    public void addEdge(int v, int w) {
+    	matrix[v][w] = 1;
+    	matrix[w][v] = 1;
+    	edges++;
+
+    }
+
+    public Iterable<Integer> adj(int v) {
+    	return null;
+    }
+
+    public boolean hasEdge(int v, int w) {
+    	return matrix[v][w] == 1;
+    }
+
+    public void print(){
+
+    	for(int i = 0; i < vertices; i++) {
+    		String str = "";
+    		for(int j = 0; j < edges; j++){
+    			str += matrix[i][j]+" ";
+
+    		}
+    		System.out.println(str);
+    	}
+    }
+
+
+
+}
+
+
+
+
+final class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String cmd = scan.nextLine();
+		int vertexNum = Integer.parseInt(scan.nextLine());
+		int edgenum = Integer.parseInt(scan.nextLine());
+		String[] places = scan.nextLine().split(",");
+        switch (cmd) {
+		case "List" :
+
+			break;
+		case "Matrix" :
+
+		     AdjMatrixGraph obj = new AdjMatrixGraph(vertexNum);
+
+		     for(int i = 0; i < edgenum; i++) {
+		     	String[] input = scan.nextLine().split(" ");
+		     	obj.addEdge(Integer.parseInt(input[0]), Integer.parseInt(input[1]));
+             }
+             obj.print();
+             break;
+
+		}
+
+
+
+
+
+	}
+}
+
+
+
 
