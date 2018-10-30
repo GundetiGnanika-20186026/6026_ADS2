998eecf5229345a03af515a8276c2e13cfd9d7e1
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-30 19:03:58 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ M2/2.1 CC_Percolation/DepthFirstPaths.java
@@ -0,0 +1,38 @@
+public class DepthFirstPaths {
+    private boolean[] marked;    // marked[v] = is there an s-v path?
+    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
+    private final int s;         // source vertex
+
+
+    public DepthFirstPaths(Graph G, int s) {
+        this.s = s;
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        validateVertex(s);
+        dfs(G, s);
+    }
+
+    // depth first search from v
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+
+
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+}
--- M2/2.1 CC_Percolation/Solution.java
+++ M2/2.1 CC_Percolation/Solution.java
@@ -1,20 +1,76 @@
 import java.util.Scanner;
+class Percolation {
+    private int size;
+    Percolation(final int vertex) {
+        size = vertex;
+    }
+    public int  transition(int i , int j) {
+        return ((i * size) + j);
+    }
+    public boolean percolates(final boolean[][] grid, final Graph graph) {
+        for (int row = 0 ; row < size ; row++) {
+            for (int col = 0 ; col < size; col++) {
+                if (grid[row][col]) {
+                    int tmp = transition(row, col);
+                    if (row == 0) {
+                        graph.addEdge(tmp , size * size);
+                    }
+                    if (row == size - 1) {
+                        graph.addEdge(tmp , size * size + 1);
+                    }
+                    if (row - 1 >= 0 && grid[row - 1][col]) {
+                        graph.addEdge(tmp , transition(row - 1, col));
+                    }
+                    if (row + 1 < size  && grid[row + 1][col]) {
+                        graph.addEdge(tmp , transition(row + 1, col));
+                    }
+                    if (col - 1 >= 0 && grid[row][col - 1]) {
+                        graph.addEdge(tmp , transition(row, col - 1));
+                    }
+                    if (col + 1 < size && grid[row][col + 1]) {
+                        graph.addEdge(tmp , transition(row, col + 1));
+                    }
+                }
+            }
+        }
+        DepthFirstPaths object = new DepthFirstPaths(graph, size * size);
+        return object.hasPathTo(size * size + 1);
+    }
 
-class Solution {
+}
+
+final class Solution {
+    private Solution(){
+
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int size = Integer.parseInt(scan.nextLine());
-        int[][] array = new int[size][size];
+        int vertexes = Integer.parseInt(scan.nextLine());
+        boolean[][] grid = new boolean[vertexes][vertexes];
+        Graph graph = new Graph(vertexes * vertexes + 2);
+        Percolation obj = new Percolation(vertexes);
+
         while (scan.hasNext()) {
-            String[] input = scan.nextLine().split(" ");
-            array[Integer.parseInt(input[0])-1][Integer.parseInt(input[1])-1] = 1;
-        }
-        for(int i = 0; i < size;i++){
-            for(int j = 0; j< size; j++){
-                System.out.print(array[i][j]+" ");
+                int rowvalue = scan.nextInt();
+                int colvalue = scan.nextInt();
+                grid[rowvalue - 1][colvalue - 1] = true;
             }
-            System.out.println();
-        }
+        System.out.println(obj.percolates(grid, graph));
+
+
+
+
+
+        // while (scan.hasNext()) {
+        //     String[] input = scan.nextLine().split(" ");
+        //     grid[Integer.parseInt(input[0])-1][Integer.parseInt(input[1])-1] = 1;
+        // }
+        // for(int i = 0; i < size;i++){
+        //     for(int j = 0; j< size; j++){
+        //         System.out.print(grid[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
 
 
 
