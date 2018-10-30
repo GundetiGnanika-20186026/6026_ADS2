60b7602f8e86fe8bce52292207ceb78163218f04
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-30 19:36:04 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M2/2.1 CC_Percolation/Graph.java
+++ M2/2.1 CC_Percolation/Graph.java
@@ -35,71 +35,126 @@ public class Graph {
         this.adj = new boolean[V][V];
     }
 
-    // number of vertices and edges
+    // and edges
 
     /**
-     * { function_description }
+     * {  number of vertices }.
      *
-     * @return     { description_of_the_return_value }
+     * @return     { v }
      */
     public int V() { return V; }
     /**
-     * { function_description }
+     * {  number of edges }.
      *
-     * @return     { description_of_the_return_value }
+     * @return     { e }
      */
     public int E() { return E; }
 
 
     // add undirected edge v-w
+
+    /**
+     * Adds an edge.
+     *
+     * @param      v     { vertex1 }
+     * @param      w     { vertex2 }
+     */
     public void addEdge(final int v, final int w) {
         if (!adj[v][w]) E++;
         adj[v][w] = true;
         adj[w][v] = true;
     }
 
-    // does the graph contain the edge v-w?
+
+
+    /**
+     * does the graph contain the edge v-w?
+     *
+     * @param      v     { vertex1 }
+     * @param      w     { vertex2 }
+     *
+     * @return     { boolean }
+     */
     public boolean contains(final int v, final int w) {
         return adj[v][w];
     }
 
-    // // return list of neighbors of v
-    // public Iterable<Integer> adj(final int v) {
-    //     return new AdjIterator(v);
-    // }
+    // return list of neighbors of v
 
-    // // support iteration over graph vertices
-    // private class AdjIterator implements Iterator<Integer>, Iterable<Integer> {
-    //     private int v;
-    //     private int w = 0;
+    /**
+     * return list of neighbors of v.
+     *
+     * @param      v     { vertex }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Integer> adj(final int v) {
+        return new AdjIterator(v);
+    }
 
-    //     AdjIterator(int v) {
-    //         this.v = v;
-    //     }
+    // support iteration over graph vertices
 
-    //     public Iterator<Integer> iterator() {
-    //         return this;
-    //     }
+    /**
+     * Class for adj iterator.
+     */
+    private class AdjIterator implements Iterator<Integer>, Iterable<Integer> {
+        /**
+         * { var_description }
+         */
+        private int v;
+        /**
+         * { var_description }
+         */
+        private int w = 0;
+        /**
+         * Constructs the object.
+         *
+         * @param      v     { parameter_description }
+         */
+        AdjIterator(int v) {
+            this.v = v;
+        }
 
-    //     public boolean hasNext() {
-    //         while (w < V) {
-    //             if (adj[v][w]) return true;
-    //             w++;
-    //         }
-    //         return false;
-    //     }
+        /**
+         * { function_description }
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Iterator<Integer> iterator() {
+            return this;
+        }
 
-    //     public Integer next() {
-    //         if (!hasNext()) {
-    //             throw new NoSuchElementException();
-    //         }
-    //         return w++;
-    //     }
+        /**
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            while (w < V) {
+                if (adj[v][w]) return true;
+                w++;
+            }
+            return false;
+        }
 
-    //     public void remove()  {
-    //         throw new UnsupportedOperationException();
-    //     }
-    // }
+        /**
+         * { function_description }
+         *
+         * @return     { description_of_the_return_value }
+         */
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return w++;
+        }
+        /**
+         * { function_description }
+         */
+        public void remove()  {
+            throw new UnsupportedOperationException();
+        }
+    }
 
 
     // string representation of Graph - takes quadratic time
