832f773bf1052ee49a4a082346ad9a8a03abf4a2
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-31 15:10:59 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M3/3.1 Directed Cycle/Digraph.java
+++ M3/3.1 Directed Cycle/Digraph.java
@@ -2,92 +2,92 @@
  * Class for digraph.
  */
 public class Digraph {
-    /**
-     *  number of vertices in this digraph.
-     */
+	/**
+	 *  number of vertices in this digraph.
+	 */
 
-    private final int V;
-    /**
-     * number of edges in this digraph.
-     */
-    private int E;
-    /**
-     * adj[v] = adjacency list for vertex v.
-     */
-    private Bag<Integer>[] adj;
-    /**
-     * indegree[v] = indegree of vertex v.
-     */
-    private int[] indegree;
-    /**
-     * Constructs the object.
-     *
-     * @param      V     { vertex }
-     */
-    public Digraph(final int V) {
+	private final int V;
+	/**
+	 * number of edges in this digraph.
+	 */
+	private int E;
+	/**
+	 * adj[v] = adjacency list for vertex v.
+	 */
+	private Bag<Integer>[] adj;
+	/**
+	 * indegree[v] = indegree of vertex v.
+	 */
+	private int[] indegree;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      V     { vertex }
+	 */
+	public Digraph(final int V) {
 
-        this.V = V;
-        this.E = 0;
-        indegree = new int[V];
-        adj = (Bag<Integer>[]) new Bag[V];
-        for (int v = 0; v < V; v++) {
-            adj[v] = new Bag<Integer>();
-        }
-    }
+		this.V = V;
+		this.E = 0;
+		indegree = new int[V];
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new Bag<Integer>();
+		}
+	}
 
-    /**
-     * returns no. of vertexes.
-     *
-     * @return     { vertexes }
-     */
-    public int V() {
-        return V;
-    }
+	/**
+	 * returns no. of vertexes.
+	 *
+	 * @return     { vertexes }
+	 */
+	public int V() {
+		return V;
+	}
 
-    /**
-     * no. of edges.
-     *
-     * @return     { edges }
-     */
-    public int E() {
-        return E;
-    }
+	/**
+	 * no. of edges.
+	 *
+	 * @return     { edges }
+	 */
+	public int E() {
+		return E;
+	}
 
-    /**
-     * Adds an edge.
-     *
-     * @param      v     { vertex1 }
-     * @param      w     { vertex2 }
-     */
-    public void addEdge(final int v, final int w) {
+	/**
+	 * Adds an edge.
+	 *
+	 * @param      v     { vertex1 }
+	 * @param      w     { vertex2 }
+	 */
+	public void addEdge(final int v, final int w) {
 
-        adj[v].add(w);
-        indegree[w]++;
-        E++;
-    }
+		adj[v].add(w);
+		indegree[w]++;
+		E++;
+	}
 
-    /**
-     * returns every value using iterator.
-     *
-     * @param      v     { vertex }
-     *
-     * @return     { a value }
-     */
-    public Iterable<Integer> adj(final int v) {
+	/**
+	 * returns every value using iterator.
+	 *
+	 * @param      v     { vertex }
+	 *
+	 * @return     { a value }
+	 */
+	public Iterable<Integer> adj(final int v) {
 
-        return adj[v];
-    }
+		return adj[v];
+	}
 
-    // public int outdegree(final int v) {
+	// public int outdegree(final int v) {
 
-    //     return adj[v].size();
-    // }
+	//     return adj[v].size();
+	// }
 
 
-    // public int indegree(final int v) {
+	// public int indegree(final int v) {
 
-    //     return indegree[v];
-    // }
+	//     return indegree[v];
+	// }
 
 
 
