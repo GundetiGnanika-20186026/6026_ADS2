8c3be75a7679b2f986efdc5ae6b5c2f43e237105
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-31 19:07:12 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M3/3.2 Bipartite/Solution.java
+++ M3/3.2 Bipartite/Solution.java
@@ -6,84 +6,81 @@ class Bipartite {
 	/**
 	 * is the graph bipartite?
 	 */
-    private boolean isBipartite;
-    /**
-     *  // color[v] gives vertices on one side of bipartition.
-     */
-    private boolean[] color;
-    /**
-     *  // marked[v] = true iff v has been visited in DFS.
-     */
-    private boolean[] marked;
-    /**
-     * // edgeTo[v] = last edge on path to v.
-     */
-    private int[] edgeTo;
-    /**
-     *  // odd-length cycle.
-     */
-    private Stack<Integer> cycle;
-    /**
-     * Constructs the object.
-     *
-     * @param      G     { graph }
-     */
-    public Bipartite(Graph g1) {
-        isBipartite = true;
-        color  = new boolean[g1.V()];
-        marked = new boolean[g1.V()];
-        edgeTo = new int[g1.V()];
+	private boolean isBipartite;
+	/**
+	 *  // color[v] gives vertices on one side of bipartition.
+	 */
+	private boolean[] color;
+	/**
+	 *  // marked[v] = true iff v has been visited in DFS.
+	 */
+	private boolean[] marked;
+	/**
+	 * // edgeTo[v] = last edge on path to v.
+	 */
+	private int[] edgeTo;
+	/**
+	 *  // odd-length cycle.
+	 */
+	private Stack<Integer> cycle;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      g1     { graph }
+	 */
+	public Bipartite(Graph g1) {
+		isBipartite = true;
+		color  = new boolean[g1.V()];
+		marked = new boolean[g1.V()];
+		edgeTo = new int[g1.V()];
 
-        for (int v = 0; v < g1.V(); v++) {
-            if (!marked[v]) {
-                dfs(g1, v);
-            }
-        }
-        //assert check(G);
-    }
+		for (int v = 0; v < g1.V(); v++) {
+			if (!marked[v]) {
+				dfs(g1, v);
+			}
+		}
 
-    private void dfs(Graph G, int v) {
-        marked[v] = true;
-        for (int w : G.adj(v)) {
+	}
 
-            // short circuit if odd-length cycle found
-            if (cycle != null) return;
+	/**
+	 * method for dfs(depth first search).
+	 *
+	 * @param      g1    The g 1
+	 * @param      v     { vertex1 }
+	 */
+	private void dfs(Graph g1, int v) {
+		marked[v] = true;
+		for (int w : g1.adj(v)) {
 
-            // found uncolored vertex, so recur
-            if (!marked[w]) {
-                edgeTo[w] = v;
-                color[w] = !color[v];
-                dfs(G, w);
-            }
+			// short circuit if odd-length cycle found
+			if (cycle != null) return;
 
-            // if v-w create an odd-length cycle, find it
-            else if (color[w] == color[v]) {
-                isBipartite = false;
-                cycle = new Stack<Integer>();
-                cycle.push(w);  // don't need this unless you want to include start vertex twice
-                for (int x = v; x != w; x = edgeTo[x]) {
-                    cycle.push(x);
-                }
-                cycle.push(w);
-            }
-        }
-    }
+			// found uncolored vertex, so recur
+			if (!marked[w]) {
+				edgeTo[w] = v;
+				color[w] = !color[v];
+				dfs(g1, w);
+			}   else if (color[w] == color[v]) {  // if v-w create an odd-length cycle, find it
+				isBipartite = false;
+				cycle = new Stack<Integer>();
+				cycle.push(w);  // don't need this unless you want to include start vertex twice
+				for (int x = v; x != w; x = edgeTo[x]) {
+					cycle.push(x);
+				}
+				cycle.push(w);
+			}
+		}
+	}
 
-    public boolean isBipartite() {
-        return isBipartite;
-    }
+	/**
+	 * Determines if bipartite.
+	 *
+	 * @return     True if bipartite, False otherwise.
+	 */
 
-
-    // public boolean color(int v1) {
-    //    // validateVertex(v);
-    //     if (!isBipartite)
-    //         throw new UnsupportedOperationException("graph is not bipartite");
-    //     return color[v1];
-    // }
-
-
-
-
+	public boolean isBipartite() {
+		return isBipartite;
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -95,7 +92,7 @@ final class Solution {
 	/**
 	 * Constructs the object.
 	 */
-	private Solution(){
+	private Solution() {
 
 	}
 	/**
@@ -108,12 +105,12 @@ final class Solution {
 		int vertexes = Integer.parseInt(scan.nextLine());
 		int edges = Integer.parseInt(scan.nextLine());
 		Graph graphobj = new Graph(vertexes);
-		while(scan.hasNext()){
-		    String[] array = scan.nextLine().split(" ");
-		    graphobj.addEdge(Integer.parseInt(array[0]), Integer.parseInt(array[1]));
+		while (scan.hasNext()) {
+			String[] array = scan.nextLine().split(" ");
+			graphobj.addEdge(Integer.parseInt(array[0]), Integer.parseInt(array[1]));
 		}
 		Bipartite b = new Bipartite(graphobj);
-		if(b.isBipartite()){
+		if (b.isBipartite()) {
 			System.out.println("Graph is bipartite");
 		} else {
 			System.out.println("Graph is not a bipartite");
