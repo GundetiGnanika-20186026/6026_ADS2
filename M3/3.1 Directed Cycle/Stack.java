fff338a6ffdfd923ec1ed820a5c343c787c4c3ae
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-10-31 17:07:41 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M3/3.1 Directed Cycle/Stack.java
+++ M3/3.1 Directed Cycle/Stack.java
@@ -1,7 +1,7 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class Stack<Item> implements Iterable<Item> {
+public class Stack<Item> {
     private int N;          // size of the stack
     private Node first;     // top of stack
 
@@ -44,57 +44,57 @@ public class Stack<Item> implements Iterable<Item> {
         N++;
     }
 
-   /**
-     * Delete and return the item most recently added to the stack.
-     * Throw an exception if no such item exists because the stack is empty.
-     */
-    public Item pop() {
-        if (isEmpty()) throw new RuntimeException("Stack underflow");
-        Item item = first.item;        // save item to return
-        first = first.next;            // delete first node
-        N--;
-        return item;                   // return the saved item
-    }
+   // /**
+   //   * Delete and return the item most recently added to the stack.
+   //   * Throw an exception if no such item exists because the stack is empty.
+   //   */
+   //  public Item pop() {
+   //      if (isEmpty()) throw new RuntimeException("Stack underflow");
+   //      Item item = first.item;        // save item to return
+   //      first = first.next;            // delete first node
+   //      N--;
+   //      return item;                   // return the saved item
+   //  }
 
 
-   /**
-     * Return the item most recently added to the stack.
-     * Throw an exception if no such item exists because the stack is empty.
-     */
-    public Item peek() {
-        if (isEmpty()) throw new RuntimeException("Stack underflow");
-        return first.item;
-    }
+   // /**
+   //   * Return the item most recently added to the stack.
+   //   * Throw an exception if no such item exists because the stack is empty.
+   //   */
+   //  public Item peek() {
+   //      if (isEmpty()) throw new RuntimeException("Stack underflow");
+   //      return first.item;
+   //  }
 
-   /**
-     * Return string representation.
-     */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (Item item : this)
-            s.append(item + " ");
-        return s.toString();
-    }
+   // /**
+   //   * Return string representation.
+   //   */
+   //  public String toString() {
+   //      StringBuilder s = new StringBuilder();
+   //      for (Item item : this)
+   //          s.append(item + " ");
+   //      return s.toString();
+   //  }
 
 
-   /**
-     * Return an iterator to the stack that iterates through the items in LIFO order.
-     */
-    public Iterator<Item> iterator()  { return new ListIterator();  }
+   // /**
+   //   * Return an iterator to the stack that iterates through the items in LIFO order.
+   //   */
+   //  public Iterator<Item> iterator()  { return new ListIterator();  }
 
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
+   //  // an iterator, doesn't implement remove() since it's optional
+   //  private class ListIterator implements Iterator<Item> {
+   //      private Node current = first;
+   //      public boolean hasNext()  { return current != null;                     }
+   //      public void remove()      { throw new UnsupportedOperationException();  }
 
-        public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            Item item = current.item;
-            current = current.next;
-            return item;
-        }
-    }
+   //      public Item next() {
+   //          if (!hasNext()) throw new NoSuchElementException();
+   //          Item item = current.item;
+   //          current = current.next;
+   //          return item;
+   //      }
+   //  }
 
 
 
