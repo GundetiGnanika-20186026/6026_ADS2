cc042cdcef3e759874c94b921565be8a15af5158
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-04 21:51:31 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M4/Code Camp - WordNet/WordNet/Digraph.java
+++ M4/Code Camp - WordNet/WordNet/Digraph.java
@@ -1,129 +1,3 @@
-// public class Digraph {
-
-//     private static final String NEWLINE = System.getProperty("line.separator");
-
-//     private final int V;           // number of vertices in this digraph
-//     private int E;                 // number of edges in this digraph
-//     private Bag<Integer>[] adj;    // adj[v] = adjacency list for vertex v
-//     private int[] indegree;        // indegree[v] = indegree of vertex v
-
-//     /**
-//      * Initializes an empty digraph with <em>V</em> vertices.
-//      *
-//      * @param  V the number of vertices
-//      * @throws IllegalArgumentException if {@code V < 0}
-//      */
-//     public Digraph(int V) {
-//         if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
-//         this.V = V;
-//         this.E = 0;
-//         indegree = new int[V];
-//         adj = (Bag<Integer>[]) new Bag[V];
-//         for (int v = 0; v < V; v++) {
-//             adj[v] = new Bag<Integer>();
-//         }
-//     }
-
-
-
-
-//     /**
-//      * Returns the number of vertices in this digraph.
-//      *
-//      * @return the number of vertices in this digraph
-//      */
-//     public int V() {
-//         return V;
-//     }
-
-//     /**
-//      * Returns the number of edges in this digraph.
-//      *
-//      * @return the number of edges in this digraph
-//      */
-//     public int E() {
-//         return E;
-//     }
-
-
-//     // throw an IllegalArgumentException unless {@code 0 <= v < V}
-//     private void validateVertex(int v) {
-//         if (v < 0 || v >= V)
-//             throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
-//     }
-
-//     /**
-//      * Adds the directed edge v→w to this digraph.
-//      *
-//      * @param  v the tail vertex
-//      * @param  w the head vertex
-//      * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
-//      */
-//     public void addEdge(int v, int w) {
-//         validateVertex(v);
-//         validateVertex(w);
-//         adj[v].add(w);
-//         indegree[w]++;
-//         E++;
-//     }
-
-//     /**
-//      * Returns the vertices adjacent from vertex {@code v} in this digraph.
-//      *
-//      * @param  v the vertex
-//      * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
-//      * @throws IllegalArgumentException unless {@code 0 <= v < V}
-//      */
-//     public Iterable<Integer> adj(int v) {
-//         validateVertex(v);
-//         return adj[v];
-//     }
-
-//     /**
-//      * Returns the number of directed edges incident from vertex {@code v}.
-//      * This is known as the <em>outdegree</em> of vertex {@code v}.
-//      *
-//      * @param  v the vertex
-//      * @return the outdegree of vertex {@code v}
-//      * @throws IllegalArgumentException unless {@code 0 <= v < V}
-//      */
-//     public int outdegree(int v) {
-//         validateVertex(v);
-//         return adj[v].size();
-//     }
-
-
-
-
-//     public boolean multipleroots() {
-//         int num = 0;
-//         for (int i = 0; i < V(); i++) {
-//             if (outdegree(i) == 0) {
-//                 num ++;
-
-//             }
-//         }
-//         if(num > 1)
-//             return true;
-//         return false;
-
-//     }
-
-//     public String toString() {
-//         StringBuilder s = new StringBuilder();
-//         s.append(V + " vertices, " + E + " edges " + NEWLINE);
-//         for (int v = 0; v < V; v++) {
-//             s.append(String.format("%d: ", v));
-//             for (int w : adj[v]) {
-//                 s.append(String.format("%d ", w));
-//             }
-//             s.append(NEWLINE);
-//         }
-//         return s.toString();
-//     }
-// }
-//
-//
 /**
  * Class for digraph.
  */
@@ -206,16 +80,22 @@ public class Digraph {
      /**
       * Returns the number of directed edges incident from vertex {@code v}.
       * This is known as the <em>outdegree</em> of vertex {@code v}.
-      *
-      * @param  v the vertex
+      *ime complexity is O(1).
+      * @param  v1 the vertex
       * @return the outdegree of vertex {@code v}
       * @throws IllegalArgumentException unless {@code 0 <= v < V}
      */
-     public int outdegree(int v) {
+     public int outdegree(final int v1) {
         //validateVertex(v);
-        return adj[v].size();
+        return adj[v1].size();
     }
 
+    /**
+     * finds if multiple roots are present.
+     * ime complexity is O(V).
+     *
+     * @return     { true or false }.
+     */
     public boolean multipleroots() {
         int num = 0;
         for (int i = 0; i < vertex(); i++) {
@@ -232,6 +112,7 @@ public class Digraph {
 
     /**
      * Returns a string representation of the object.
+     *ime complexity is O(V+E).
      *
      * @return     String representation of the object.
      */
