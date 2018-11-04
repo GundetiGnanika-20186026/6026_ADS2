4e002206c8bbf6cc891c4b9e34ef0d336c19431a
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-04 21:34:42 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M4/Code Camp - WordNet/WordNet/Digraph.java
+++ M4/Code Camp - WordNet/WordNet/Digraph.java
@@ -193,16 +193,16 @@ public class Digraph {
     //  * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
     //  *         followed by the <em>V</em> adjacency lists
     //  */
-    // public String toString() {
-    //     StringBuilder s = new StringBuilder();
-    //     s.append(V + " vertices, " + E + " edges " + NEWLINE);
-    //     for (int v = 0; v < V; v++) {
-    //         s.append(String.format("%d: ", v));
-    //         for (int w : adj[v]) {
-    //             s.append(String.format("%d ", w));
-    //         }
-    //         s.append(NEWLINE);
-    //     }
-    //     return s.toString();
-    // }
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(String.format("%d: ", v));
+            for (int w : adj[v]) {
+                s.append(String.format("%d ", w));
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
 }
