80819e09bee460dff3ae51ab9db1bcf666351bfd
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-04 17:16:20 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M4/Code Camp - WordNet/WordNet/SAP.java
+++ M4/Code Camp - WordNet/WordNet/SAP.java
@@ -1,33 +1,52 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Arrays;
+/**
+ * Class for sap.
+ */
 public class SAP {
-
-
-
-    Digraph graph;
+    /**
+     * graph
+     */
+    private Digraph graph;
 
     // constructor takes a digraph (not necessarily a DAG)
+
+    /**
+     * Constructs the object.
+     *
+     * @param      graph  The graph
+     */
     public SAP(Digraph graph) {
         this.graph = graph;
 
     }
 
+
+
+    /**
     // length of shortest ancestral path between v and w; -1 if no such path
-    public void length(ArrayList<Integer> arraylist1, ArrayList<Integer> arraylist2, HashMap<Integer, String[]> synset) {
+     *
+     *
+     * @param      arraylist1  The arraylist 1
+     * @param      arraylist2  The arraylist 2
+     * @param      synset      The synset
+     */
+    public void length(ArrayList<Integer> arraylist1, ArrayList<Integer> arraylist2,
+                       HashMap<Integer, String[]> synset) {
         int distance = 0;
-        int m = Integer.MAX_VALUE;
+        int max = Integer.MAX_VALUE;
         String[] a = null;
         for (int i = 0; i < arraylist1.size(); i++) {
             for (int j = 0; j < arraylist2.size(); j++) {
-                BreadthFirstPath bfsVer1 = new BreadthFirstPath(graph, arraylist1.get(i));
-                BreadthFirstPath bfsVer2 = new BreadthFirstPath(graph, arraylist2.get(j));
+BreadthFirstPath bfsVer1 = new BreadthFirstPath(graph, arraylist1.get(i));
+BreadthFirstPath bfsVer2 = new BreadthFirstPath(graph, arraylist2.get(j));
                 for (int k = 0; k < graph.V(); k++) {
                     if (bfsVer1.hasPathTo(k) && bfsVer2.hasPathTo(k)) {
                         distance = bfsVer1.distTo(k) + bfsVer2.distTo(k);
                         // System.out.println(distance);
-                        if (distance < m) {
-                            m = distance;
+                        if (distance < max) {
+                            max = distance;
                             a = synset.get(k);
 
                         }
@@ -45,7 +64,7 @@ public class SAP {
         ancestor.append(a[a.length - 1]);
 
 
-        System.out.println("distance = " + m + ", ancestor = " + ancestor);
+        System.out.println("distance = " + max + ", ancestor = " + ancestor);
 
 
 
