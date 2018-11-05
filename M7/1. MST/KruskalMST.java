f60ee87e3f0e90dbc68566120a85ca8c7105c632
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-05 20:46:44 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M7/1. MST/KruskalMST.java
+++ M7/1. MST/KruskalMST.java
@@ -29,7 +29,7 @@ public class KruskalMST {
         }
 
         // check optimality conditions
-        assert check(G);
+       // assert check(G);
     }
 
     /**
@@ -50,78 +50,61 @@ public class KruskalMST {
     }
 
     // check optimality conditions (takes time proportional to E V lg* V)
-    private boolean check(EdgeWeightedGraph G) {
+    // private boolean check(EdgeWeightedGraph G) {
 
-        // check total weight
-        double total = 0.0;
-        for (Edge e : edges()) {
-            total += e.weight();
-        }
-        if (Math.abs(total - weight()) > FLOATING_POINT_EPSILON) {
-            System.err.printf("Weight of edges does not equal weight(): %f vs. %f\n", total, weight());
-            return false;
-        }
-
-        // check that it is acyclic
-        UF uf = new UF(G.V());
-        for (Edge e : edges()) {
-            int v = e.either(), w = e.other(v);
-            if (uf.connected(v, w)) {
-                System.err.println("Not a forest");
-                return false;
-            }
-            uf.union(v, w);
-        }
-
-        // check that it is a spanning forest
-        for (Edge e : G.edges()) {
-            int v = e.either(), w = e.other(v);
-            if (!uf.connected(v, w)) {
-                System.err.println("Not a spanning forest");
-                return false;
-            }
-        }
-
-        // check that it is a minimal spanning forest (cut optimality conditions)
-        for (Edge e : edges()) {
-
-            // all edges in MST except e
-            uf = new UF(G.V());
-            for (Edge f : mst) {
-                int x = f.either(), y = f.other(x);
-                if (f != e) uf.union(x, y);
-            }
-
-            // check that e is min weight edge in crossing cut
-            for (Edge f : G.edges()) {
-                int x = f.either(), y = f.other(x);
-                if (!uf.connected(x, y)) {
-                    if (f.weight() < e.weight()) {
-                        System.err.println("Edge " + f + " violates cut optimality conditions");
-                        return false;
-                    }
-                }
-            }
-
-        }
-
-        return true;
-    }
-
-
-    // /**
-    //  * Unit tests the {@code KruskalMST} data type.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     In in = new In(args[0]);
-    //     EdgeWeightedGraph G = new EdgeWeightedGraph(in);
-    //     KruskalMST mst = new KruskalMST(G);
-    //     for (Edge e : mst.edges()) {
-    //         StdOut.println(e);
+    //     // check total weight
+    //     double total = 0.0;
+    //     for (Edge e : edges()) {
+    //         total += e.weight();
     //     }
-    //     StdOut.printf("%.5f\n", mst.weight());
-    // }
+    //     if (Math.abs(total - weight()) > FLOATING_POINT_EPSILON) {
+    //         System.err.printf("Weight of edges does not equal weight(): %f vs. %f\n", total, weight());
+    //         return false;
+    //     }
 
+    //     // check that it is acyclic
+    //     UF uf = new UF(G.V());
+    //     for (Edge e : edges()) {
+    //         int v = e.either(), w = e.other(v);
+    //         if (uf.connected(v, w)) {
+    //             System.err.println("Not a forest");
+    //             return false;
+    //         }
+    //         uf.union(v, w);
+    //     }
+
+    //     // check that it is a spanning forest
+    //     for (Edge e : G.edges()) {
+    //         int v = e.either(), w = e.other(v);
+    //         if (!uf.connected(v, w)) {
+    //             System.err.println("Not a spanning forest");
+    //             return false;
+    //         }
+    //     }
+
+    //     // check that it is a minimal spanning forest (cut optimality conditions)
+    //     for (Edge e : edges()) {
+
+    //         // all edges in MST except e
+    //         uf = new UF(G.V());
+    //         for (Edge f : mst) {
+    //             int x = f.either(), y = f.other(x);
+    //             if (f != e) uf.union(x, y);
+    //         }
+
+    //         // check that e is min weight edge in crossing cut
+    //         for (Edge f : G.edges()) {
+    //             int x = f.either(), y = f.other(x);
+    //             if (!uf.connected(x, y)) {
+    //                 if (f.weight() < e.weight()) {
+    //                     System.err.println("Edge " + f + " violates cut optimality conditions");
+    //                     return false;
+    //                 }
+    //             }
+    //         }
+
+    //     }
+
+    //     return true;
+    // }
 }
