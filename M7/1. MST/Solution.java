3bccbe4ae0a9c3937c9b671ba06f5bcee8ab47e2
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-05 20:27:03 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M7/1. MST/Solution.java
+++ M7/1. MST/Solution.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertexes = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertexes = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
         EdgeWeightedGraph weighted = new EdgeWeightedGraph(vertexes);
 
-		for(int i = 0;i<edges;i++) {
-			String[] input = scan.nextLine().split(" ");
+        for(int i = 0;i<edges;i++) {
+            String[] input = scan.nextLine().split(" ");
             Edge edgeobj = new Edge(Integer.parseInt(input[0]),
              Integer.parseInt(input[1]), Double.parseDouble(input[2]));
             weighted.addEdge(edgeobj);
