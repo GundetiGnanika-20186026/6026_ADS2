d73ae9bf87fe71ed18f0d09f97840ac2ce6c5d97
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-06 17:42:50 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M8/1. Shortest Paths/Solution.java
+++ M8/1. Shortest Paths/Solution.java
@@ -4,44 +4,44 @@ import java.util.HashMap;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution(){
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		HashMap<String, Integer> hashobj = new HashMap<String, Integer>();
-		Scanner scan = new Scanner(System.in);
-		String[] num = scan.nextLine().split(" ");
-		int vertexes = Integer.parseInt(num[0]);
-		int edges = Integer.parseInt(num[1]);
-		String[] places = scan.nextLine().split(" ");
-		EdgeWeightedGraph graphobj = new EdgeWeightedGraph(vertexes);
-		for(int i = 0; i<places.length;i++) {
-			hashobj.put(places[i], i);
-		}
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+HashMap<String, Integer> hashobj = new HashMap<String, Integer>();
+        Scanner scan = new Scanner(System.in);
+        String[] num = scan.nextLine().split(" ");
+        int vertexes = Integer.parseInt(num[0]);
+        int edges = Integer.parseInt(num[1]);
+        String[] places = scan.nextLine().split(" ");
+EdgeWeightedGraph graphobj = new EdgeWeightedGraph(vertexes);
+        for (int i = 0; i < places.length; i++) {
+            hashobj.put(places[i], i);
+        }
 
 
 
-		for (int i = 0; i < edges; i++) {
-			String[] formation = scan.nextLine().split(" ");
-	        Edge edgeobj = new Edge(hashobj.get(formation[0]),
-			 hashobj.get(formation[1]), Double.parseDouble(formation[2]));
-			graphobj.addEdge(edgeobj);
+        for (int i = 0; i < edges; i++) {
+            String[] formation = scan.nextLine().split(" ");
+            Edge edgeobj = new Edge(hashobj.get(formation[0]),
+   hashobj.get(formation[1]), Double.parseDouble(formation[2]));
+            graphobj.addEdge(edgeobj);
         }
 
         int queries = Integer.parseInt(scan.nextLine());
-        for(int i =0;i<queries;i++){
-        	String[] qur = scan.nextLine().split(" ");
-        	DijkstraSP dis = new DijkstraSP(graphobj, hashobj.get(qur[0]));
-        	System.out.println((int)dis.distance(hashobj.get(qur[1])));
+        for (int i = 0; i < queries; i++) {
+            String[] qur = scan.nextLine().split(" ");
+    DijkstraSP dis = new DijkstraSP(graphobj, hashobj.get(qur[0]));
+    System.out.println((int) dis.distance(hashobj.get(qur[1])));
         }
 
-	}
+    }
 }
