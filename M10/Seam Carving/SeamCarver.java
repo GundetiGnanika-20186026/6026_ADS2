3f4c8f43bc4c3951c26c4f1626cfa6300df84572
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-08 10:57:52 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M10/Seam Carving/SeamCarver.java
+++ M10/Seam Carving/SeamCarver.java
@@ -5,11 +5,11 @@ public class SeamCarver {
 	// create a seam carver object based on the given picture
 	public SeamCarver(Picture picture) {
 		this.pic = picture;
-		if(picture == null) {
+		if (picture == null) {
 			System.out.println("picture is null");
 		}
 
-    }
+	}
 	// current picture
 	public Picture picture() {
 		return null;
@@ -28,7 +28,7 @@ public class SeamCarver {
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
 		//System.out.print(pic.getRGB(x, y));
-		if(x==0||y==0||x==height()||y==width()-1){
+		if (x == 0 || y == 0 || x == width() || y == height()) {
 			return 1000;
 		}
 		return 0;
