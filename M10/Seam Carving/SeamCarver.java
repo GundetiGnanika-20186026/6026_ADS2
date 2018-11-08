100d1a1532d7747a323ed7a322a67a1488e0f5e6
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-08 11:58:23 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M10/Seam Carving/SeamCarver.java
+++ M10/Seam Carving/SeamCarver.java
@@ -28,11 +28,21 @@ public class SeamCarver {
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
 		//System.out.print(pic.getRGB(x, y));
-		if (x == 0 || y == 0 || x == width()-1 || y == height()-1) {
+		if (x == 0 || y == 0 || x == width() - 1 || y == height() - 1) {
 			return 1000;
 		}
-		System.out.println(pic.get(x, y));
-		return 0;
+		// pic.get(x, y).getRed();
+		// System.out.println();
+		int rx = pic.get(x, y - 1).getRed() - pic.get(x, y + 1).getRed();
+		int gx = pic.get(x, y - 1).getGreen() - pic.get(x, y + 1).getGreen();
+		int bx = pic.get(x, y - 1).getBlue() - pic.get(x, y + 1).getBlue();
+		double xsum = (rx * rx) + (gx * gx) + (bx * bx);
+		int ry = pic.get(x - 1, y).getRed() - pic.get(x + 1, y).getRed();
+		int gy = pic.get(x - 1, y).getGreen() - pic.get(x + 1, y).getGreen();
+		int by = pic.get(x - 1, y).getBlue() - pic.get(x + 1, y).getBlue();
+		double ysum = (ry * ry) + (gy * gy) + (by * by);
+		double res = Math.sqrt(xsum + ysum);
+		return res;
 	}
 
 	// sequence of indices for horizontal seam
