7d4877f9ca1caa2e6b67ebc8c177f825a64c4304
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-08 11:02:51 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M10/Seam Carving/SeamCarver.java
+++ M10/Seam Carving/SeamCarver.java
@@ -28,7 +28,7 @@ public class SeamCarver {
 	// energy of pixel at column x and row y
 	public double energy(int x, int y) {
 		//System.out.print(pic.getRGB(x, y));
-		if (x == 0 || y == 0 || x == width() || y == height()) {
+		if (x == 0 || y == 0 || x == width()-1 || y == height()-1) {
 			return 1000;
 		}
 		return 0;
