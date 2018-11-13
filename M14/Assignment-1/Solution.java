f3cf9b57d19d6e727c37408242df390252e85e55
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-13 17:14:53 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M14/Assignment-1/Solution.java
+++ M14/Assignment-1/Solution.java
@@ -1,32 +1,65 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
 
-public class Solution {
+	}
+	/**
+	 * main method.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Tst<Integer> st = new Tst<Integer>();
 		String[] words = loadWords();
-
-		//Your code goes here...
 		int j = 1;
 		for (int i = 0; i < words.length; i++) {
-            for(int k = 0;k<words[i].length();k++) {
-			    st.put(words[i].substring(k), j);
-			     j++;
-		   }
+			for (int k = 0; k < words[i].length(); k++) {
+				st.put(words[i].substring(k), j);
+				j++;
+			}
 		}
 		String req = scan.nextLine();
-		//int sum = 0;
+
 		for (String s : st.keysWithPrefix(req)) {
 			System.out.println(s);
-			//sum +=1;
+
 		}
-		//System.out.println(sum);
+    }
 
-	}
-
+	/**
+	 * Loads words.
+	 *
+	 * @return     { array with words }.
+	 */
 	public static String[] loadWords() {
+
+		// try {
+		// 	Scanner filescan = new Scanner(new File("/Files/dictionary-algs4.txt"));
+		// 	String[] input = new String[6013];
+
+		// 	int s = 0;
+		// 	while (filescan.hasNextLine()) {
+		// 		input[s] = filescan.nextLine();
+		// 		s++;
+		// 	}
+		// 	return input;
+
+		// } catch (FileNotFoundException e) {
+		// 	System.out.println("file not found");
+		// }
+
 		In in = new In("/Files/dictionary-algs4.txt");
 		String[] words = in.readAllStrings();
 		return words;
+		//return null;
 	}
 }
