ee49ef7e70f865836a153780398139d22e39108f
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-13 16:37:26 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M14/Assignment-1/Solution.java
+++ M14/Assignment-1/Solution.java
@@ -2,18 +2,23 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
-		 Scanner scan = new Scanner(System.in);
-		 Tst<Integer> st = new Tst<Integer>();
+		Scanner scan = new Scanner(System.in);
+		Tst<Integer> st = new Tst<Integer>();
 		String[] words = loadWords();
+
 		//Your code goes here...
-		for(int i = 0;i< words.length;i++){
-			st.put(words[i], i);
+		int j = 1;
+		for (int i = 0; i < words.length; i++) {
+            for(int k = 0;k<words[i].length();k++) {
+			    st.put(words[i].substring(k), j);
+			     j++;
+		   }
 		}
-        String req = scan.nextLine();
-        //int sum = 0;
+		String req = scan.nextLine();
+		//int sum = 0;
 		for (String s : st.keysWithPrefix(req)) {
-            System.out.println(s);
-            //sum +=1;
+			System.out.println(s);
+			//sum +=1;
 		}
 		//System.out.println(sum);
 
