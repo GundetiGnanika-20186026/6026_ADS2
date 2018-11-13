a160bfa04e8d8d7ae77a7f38c630fc53c123a51e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-11-13 17:32:17 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M14/Assignment-1/Solution.java
+++ M14/Assignment-1/Solution.java
@@ -1,65 +1,66 @@
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
+//import java.io.File;
+//import java.io.FileNotFoundException;
 /**
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Tst<Integer> st = new Tst<Integer>();
-		String[] words = loadWords();
-		int j = 1;
-		for (int i = 0; i < words.length; i++) {
-			for (int k = 0; k < words[i].length(); k++) {
-				st.put(words[i].substring(k), j);
-				j++;
-			}
-		}
-		String req = scan.nextLine();
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Tst<Integer> st = new Tst<Integer>();
+        String[] words = loadWords();
+        int j = 1;
+        for (int i = 0; i < words.length; i++) {
+            for (int k = 0; k < words[i].length(); k++) {
+                st.put(words[i].substring(k), j);
+                j++;
+            }
+        }
+        String req = scan.nextLine();
 
-		for (String s : st.keysWithPrefix(req)) {
-			System.out.println(s);
+        for (String s : st.keysWithPrefix(req)) {
+            System.out.println(s);
 
-		}
+        }
     }
 
-	/**
-	 * Loads words.
-	 *
-	 * @return     { array with words }.
-	 */
-	public static String[] loadWords() {
+    /**
+     * Loads words.
+     *
+     * @return     { array with words }.
+     */
+    public static String[] loadWords() {
 
-		// try {
-		// 	Scanner filescan = new Scanner(new File("/Files/dictionary-algs4.txt"));
-		// 	String[] input = new String[6013];
+        // try {
+        //  Scanner filescan = new Scanner
+        //  (new File("/Files/dictionary-algs4.txt"));
+        //  String[] input = new String[6013];
 
-		// 	int s = 0;
-		// 	while (filescan.hasNextLine()) {
-		// 		input[s] = filescan.nextLine();
-		// 		s++;
-		// 	}
-		// 	return input;
+        //  int s = 0;
+        //  while (filescan.hasNextLine()) {
+        //      input[s] = filescan.nextLine();
+        //      s++;
+        //  }
+        //  return input;
 
-		// } catch (FileNotFoundException e) {
-		// 	System.out.println("file not found");
-		// }
+        // } catch (FileNotFoundException e) {
+        //  System.out.println("file not found");
+        // }
 
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-		//return null;
-	}
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+        //return null;
+    }
 }
